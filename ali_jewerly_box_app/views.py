1c7441a43f2138df497cdb53af7f91fd137cdf3f
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-07-16 13:04:24 +0300

    create the request methods for product module

--- ali_jewerly_box_app/views.py
+++ ali_jewerly_box_app/views.py
@@ -1,10 +1,22 @@
+from importlib.resources import files
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import *
 from .serializers import * 
 
 # Create your views here.
-
-def home(request):
-    return render(request, 'home.html')
+@api_view(['GET', 'POST'])
+def products(request):
+    if request.method == 'GET':
+        prod = Product.objects.all()
+        prod_serializ = ProductSerializer(prod, many=True)
+        return Response(prod_serializ.data)
+    elif request.method == 'POST':
+        prod_serializ= ProductSerializer(data = request.data, files = request.FILES)
+        if prod_serializ.is_valid():
+            prod_serializ.save()
+            return Response(prod_serializ.data, status=status.HTTP_201_CREATED)
+        return Response(prod_serializ.errors, status=status.HTTP_400_BAD_REQUEST)
+    
