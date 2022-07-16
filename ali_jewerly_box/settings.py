7b39d8da96e05ec6b8e2ade85a4e4e499a4c0335
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-07-16 13:58:35 +0300

    install corse headers to allow domain sharing

--- ali_jewerly_box/settings.py
+++ ali_jewerly_box/settings.py
@@ -54,9 +54,11 @@ INSTALLED_APPS = [
     'ali_jewerly_box_app',
     'rest_framework',
     'cloudinary',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -146,3 +148,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#configuring corsheaders and csrf
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+CSRF_TRUSTED_ORIGINS = [
+    "http://loacalhost:3000",
+]
